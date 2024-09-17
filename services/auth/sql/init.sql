6d26bfe2c325dc5babfb905afd2ef66b52d50ff9
Terence O'Donoghue <1697905+terenceodonoghue@users.noreply.github.com>
2024-09-17 20:19:32 +0800

    Update init.sql

--- services/auth/sql/init.sql
+++ services/auth/sql/init.sql
@@ -1,27 +1,33 @@
 CREATE TABLE ext_providers (
-	ext_provider_id serial PRIMARY KEY,
+	ext_provider_id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 	provider_name text CHECK (char_length(provider_name) <= 50) UNIQUE
 );
 
 CREATE TABLE user_accounts (
-	user_account_id serial PRIMARY KEY,
+	user_account_id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
 	first_name text CHECK (char_length(last_name) <= 50),
 	last_name text CHECK (char_length(last_name) <= 50)
 );
 
+CREATE TABLE user_sessions (
+	user_session_id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+	user_account_id bigint NOT NULL,
+	FOREIGN KEY (user_account_id) REFERENCES user_accounts (user_account_id) ON DELETE CASCADE
+);
+
 CREATE TABLE user_credentials (
-	user_account_id int PRIMARY KEY,
-	email_address text CHECK (char_length(email_address) <= 50) UNIQUE,
+	user_account_id bigint PRIMARY KEY,
+	email_address text CHECK (char_length(email_address) <= 50) UNIQUE NOT NULL,
 	password_hash text CHECK (char_length(password_hash) <= 250) NOT NULL,
 	password_salt text CHECK (char_length(password_salt) <= 100) NOT NULL,
-	FOREIGN KEY (user_account_id) REFERENCES user_accounts (user_account_id)
+	FOREIGN KEY (user_account_id) REFERENCES user_accounts (user_account_id) ON DELETE CASCADE
 );
 
 CREATE TABLE user_credentials_ext (
-	user_account_id int,
-	ext_provider_id int,
+	user_account_id bigint,
+	ext_provider_id bigint,
 	ext_provider_token text CHECK (char_length(ext_provider_token) <= 100) NOT NULL,
-	FOREIGN KEY (user_account_id) REFERENCES user_accounts (user_account_id),
-	FOREIGN KEY (ext_provider_id) REFERENCES ext_providers (ext_provider_id),
+	FOREIGN KEY (user_account_id) REFERENCES user_accounts (user_account_id) ON DELETE CASCADE,
+	FOREIGN KEY (ext_provider_id) REFERENCES ext_providers (ext_provider_id) ON DELETE CASCADE,
 	PRIMARY KEY (user_account_id, ext_provider_id)
 );
