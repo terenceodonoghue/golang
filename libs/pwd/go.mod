e7d535fb5170cdc9c1da53afdd627ca9dc2d9b96
Terence O'Donoghue <1697905+terenceodonoghue@users.noreply.github.com>
2024-10-10 08:30:09 +1100

    Update pwd lib

--- libs/pwd/go.mod
+++ libs/pwd/go.mod
@@ -2,4 +2,4 @@ module github.com/terenceodonoghue/golang/libs/pwd
 
 go 1.23.1
 
-require golang.org/x/crypto v0.27.0 // indirect
+require golang.org/x/crypto v0.27.0
--- libs/pwd/pwd.go
+++ libs/pwd/pwd.go
@@ -1,1 +1,15 @@
 package pwd
+
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
+func Compare(hash []byte, password []byte) bool {
+	err := bcrypt.CompareHashAndPassword(hash, password)
+	return err == nil
+}
+
+func HashAndSalt(password []byte) ([]byte, error) {
+	hash, err := bcrypt.GenerateFromPassword(password, bcrypt.MinCost)
+	return hash, err
+}
